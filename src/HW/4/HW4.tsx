e28d725866a732aae839d2bc37b14e5f2f1dda26
vladseme2012 <vlad.seme2012@yandex.ru>
2025-01-21 16:58:54 +0300

    corected styles in li

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -35,7 +35,7 @@ export const HW4 = () => {
                         <li
                             key={index}
                             id={`hw04-task-${index}`}
-                            className={(index % 2) ? s.chetNechet : ''}
+                            className={index % 2 ? '' : s.chetNechet}
                         >
                             {el}
                         </li>
